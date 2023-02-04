d2aab0d30bd3f1c392380ae647d10f5625429102
jugreil <juliusgreil@gmail.com>
2023-02-04 17:54:07 +0100

    initialize

---
+++ VueOlympics-master/client/src/components/TableComp.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <DataTable class="table table−hover table−striped" width="100%" :data="medals">
+    <thead>
+      <tr>
+        Hi
+      </tr>
+    </thead>
+    <tfoot>
+      No
+    </tfoot>
+  </DataTable>
+</template>
+
+<script setup>
+import DataTable from "datatables.net-vue3";
+</script>
+
+<style scoped>
+@import "bootstrap";
+@import "datatables.net-bs5";
+</style>
